5928f7af8e78f7422813e07620419ea75562f990
Turbobot <turbobot@vercel.com>
2023-09-05 16:00:30 +0800

    feat(create-turbo): apply official-starter transform

--- prisma/migrations/20230823022513_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "NftData" (
-    "tokenId" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "userAddress" TEXT NOT NULL,
-    "recepit" TEXT NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "NftData_recepit_key" ON "NftData"("recepit");
