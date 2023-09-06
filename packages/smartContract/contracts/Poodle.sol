7b021e5948e2a7123ce03518f0077c747862ffdb
abhishekkochar <abhishekkochar2@gmail.com>
2023-09-06 13:51:22 +0800

    Added smart contract to monorepo
    Signed-off-by: abhishekkochar <abhishekkochar2@gmail.com>

---
+++ packages/smartContract/contracts/Poodle.sol
@@ -0,0 +1,29 @@
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+contract Poodle is ERC721, Ownable {
+    // Revert if the msg.sender is an owner
+    error OWNER_NOT_ALLOWED_TO_MINT();
+
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
+
+    /**
+     * External function to mint NFT
+     * @param to, address to send the nft to 
+     * @param tokenId, id to mint
+     * Owner is not allowed to mint Poodle NFT
+     */
+    function mint(address to, uint256 tokenId) external {
+        if(msg.sender == owner()) revert OWNER_NOT_ALLOWED_TO_MINT();
+        _mint(to, tokenId);
+    }
+
+    /**
+     * External function to send tokenId to
+     * @param to, address to send the nft to
+     * @param tokenId, Id to transfer
+     */
+    function transfer(address to, uint256 tokenId) external {
+        _transfer(msg.sender, to, tokenId);
+    }
+}
